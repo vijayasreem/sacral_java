153da033eda15f3276902dd4ed5c152fd6b47cf6
vijayasreem <133225735+vijayasreem@users.noreply.github.com>
2023-06-05 18:23:12 +0530

    Updated

--- pojo/TestTestUserStory.java
+++ pojo/TestTestUserStory.java
@@ -1,15 +1,5 @@
 public class TestTestUserStory {
-    private String story;
-
-    public TestTestUserStory(String story) {
-        this.story = story;
-    }
-
-    public String getStory() {
-        return story;
-    }
-
-    public void setStory(String story) {
-        this.story = story;
+    public static void main(String[] args) {
+        System.out.println("Test Test User Story");
     }
 }
