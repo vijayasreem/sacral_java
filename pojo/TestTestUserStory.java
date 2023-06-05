8e0bb85cc1e5dabf273bf5a97a49922bde7a34bb
vijayasreem <133225735+vijayasreem@users.noreply.github.com>
2023-06-05 18:23:09 +0530

    initial commit

---
+++ pojo/TestTestUserStory.java
@@ -0,0 +1,15 @@
+public class TestTestUserStory {
+    private String story;
+
+    public TestTestUserStory(String story) {
+        this.story = story;
+    }
+
+    public String getStory() {
+        return story;
+    }
+
+    public void setStory(String story) {
+        this.story = story;
+    }
+}
